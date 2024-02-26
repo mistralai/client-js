10d3e1abcc13cd0c6bbc60275d91b68af3d00a6c
Bam4d <chrisbam4d@gmail.com>
2024-02-26 14:18:38 +0000

    rename to correct name

--- examples/json_format.js
+++ examples/json_format.js
@@ -5,7 +5,7 @@ const apiKey = process.env.MISTRAL_API_KEY;
 const client = new MistralClient(apiKey);
 
 const chatResponse = await client.chat({
-  model: 'mistral-large-bouffe',
+  model: 'mistral-large',
   messages: [{role: 'user', content: 'What is the best French cheese?'}],
   responseFormat: {type: "json_object"}
 });
