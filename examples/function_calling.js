38fda09da70148c5619580d972712c29c556f1c1
Alexis Tacnet <alexistacnet@gmail.com>
2024-05-23 10:01:37 +0200

    Add tool call id in TS and example for v3 tokenization

--- examples/function_calling.js
+++ examples/function_calling.js
@@ -6,9 +6,13 @@ const apiKey = process.env.MISTRAL_API_KEY;
 const data = {
   transactionId: ['T1001', 'T1002', 'T1003', 'T1004', 'T1005'],
   customerId: ['C001', 'C002', 'C003', 'C002', 'C001'],
-  paymentAmount: [125.50, 89.99, 120.00, 54.30, 210.20],
+  paymentAmount: [125.5, 89.99, 120.0, 54.3, 210.2],
   paymentDate: [
-    '2021-10-05', '2021-10-06', '2021-10-07', '2021-10-05', '2021-10-08',
+    '2021-10-05',
+    '2021-10-06',
+    '2021-10-07',
+    '2021-10-05',
+    '2021-10-08',
   ],
   paymentStatus: ['Paid', 'Unpaid', 'Paid', 'Paid', 'Pending'],
 };
@@ -60,8 +64,8 @@ const tools = [
       parameters: {
         type: 'object',
         required: ['transactionId'],
-        properties: {transactionId:
-          {type: 'string', description: 'The transaction id.'},
+        properties: {
+          transactionId: {type: 'string', description: 'The transaction id.'},
         },
       },
     },
@@ -74,36 +78,41 @@ const tools = [
       parameters: {
         type: 'object',
         required: ['transactionId'],
-        properties: {transactionId:
-          {type: 'string', description: 'The transaction id.'},
+        properties: {
+          transactionId: {type: 'string', description: 'The transaction id.'},
         },
       },
     },
   },
 ];
 
+const model = 'mistral-small-latest';
 
-const model = 'mistral-large';
-
-const client = new MistralClient(apiKey, 'https://api-2.aurocloud.net');
+const client = new MistralClient(apiKey);
 
 const messages = [
   {role: 'user', content: 'What\'s the status of my transaction?'},
 ];
 
 let response = await client.chat({
-  model: model, messages: messages, tools: tools,
+  model: model,
+  messages: messages,
+  tools: tools,
 });
-
 
 console.log(response.choices[0].message.content);
 
-messages.push(
-  {role: 'assistant', content: response.choices[0].message.content},
-);
+messages.push({
+  role: 'assistant',
+  content: response.choices[0].message.content,
+});
 messages.push({role: 'user', content: 'My transaction ID is T1001.'});
 
-response = await client.chat({model: model, messages: messages, tools: tools});
+response = await client.chat({
+  model: model,
+  messages: messages,
+  tools: tools,
+});
 
 const toolCall = response.choices[0].message.toolCalls[0];
 const functionName = toolCall.function.name;
@@ -115,8 +124,17 @@ console.log(`functionParams: ${toolCall.function.arguments}`);
 const functionResult = namesToFunctions[functionName](functionParams);
 
 messages.push(response.choices[0].message);
-messages.push({role: 'tool', name: functionName, content: functionResult});
+messages.push({
+  role: 'tool',
+  name: functionName,
+  content: functionResult,
+  tol_call_id: toolCall.id,
+});
 
-response = await client.chat({model: model, messages: messages, tools: tools});
+response = await client.chat({
+  model: model,
+  messages: messages,
+  tools: tools,
+});
 
 console.log(response.choices[0].message.content);
